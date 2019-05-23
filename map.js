cd21cb9636a515100ab92e71dc6b35866c7366b0
moonfleur <katenkak551@gmail.com>
2019-05-24 00:00:47 +0300

    map

--- map.js
+++ map.js
@@ -20,12 +20,12 @@ function initMap() {
     var searchBox = new google.maps.places.SearchBox(input);
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-    map.addEventListener('bounds_changed', function() {
+    map.addListener('bounds_changed', function() {
         searchBox.setBounds(map.getBounds());
     });
 
     var markers = [];
-    searchBox.addEventListener('places_changed', function() {
+    searchBox.addListener('places_changed', function() {
         places = searchBox.getPlaces();
 
         if (places.length === 0) {
@@ -67,83 +67,3 @@ function initMap() {
         map.fitBounds(bounds);
     });
 }
-
-// function initMap() {
-//     map = new google.maps.Map(document.getElementById('map'), {
-//         center: center,
-//         zoom: 12,
-//         mapTypeId: google.maps.MapTypeId.ROADMAP
-//     });
-//
-//     // var marker = new google.maps.Marker({
-//     //     position: {
-//     //         lat: lat,
-//     //         lng: lng
-//     //     },
-//     //     map: map
-//     // });
-//
-//     // infowindow = new google.maps.InfoWindow();
-//     //
-//     // var request = {
-//     //     query: 'library',
-//     //     fields: ['name', 'geometry'],
-//     //     type: 'libraries'
-//     // };
-//     //
-//     // var service = new google.maps.places.PlacesService(map);
-//     //
-//     // service.findPlaceFromQuery(request, function(results, status) {
-//     //     if (status === google.maps.places.PlacesServiceStatus.OK) {
-//     //         for (var i = 0; i < results.length; i++) {
-//     //             createMarker(results[i]);
-//     //         }
-//     //         map.setCenter(results[0].geometry.location);
-//     //     }
-//     // });
-//     //
-//     // function createMarker(place) {
-//     //     var marker = new google.maps.Marker({
-//     //         map: map,
-//     //         position: place.geometry.location
-//     //     });
-//     //
-//     //     google.maps.event.addEventListener(marker, 'click', function () {
-//     //         infowindow.setContent(place.name);
-//     //         infowindow.open(map, this);
-//     //     });
-//     // }
-//
-//     infowindow = new google.maps.InfoWindow();
-//
-//     var request = {
-//         location: center,
-//         radius: '500',
-//         type: ['lodging'],
-//         query: 'libraries'
-//     };
-//
-//     service = new google.maps.places.PlacesService(map);
-//     service.nearbySearch(request, callback);
-// }
-//
-// function callback(results, status) {
-//     if (status === google.maps.places.PlacesServiceStatus.OK) {
-//         for (var i = 0; i < results.length; i++) {
-//             place = results[i];
-//             createMarker(results[i]);
-//         }
-//     }
-// }
-//
-// function createMarker(place) {
-//     var marker = new google.maps.Marker({
-//         map: map,
-//         position: place.geometry.location
-//     });
-//
-//     google.maps.event.addEventListener(marker, 'click', function () {
-//         infowindow.setContent(place.name);
-//         infowindow.open(map, this);
-//     });
-// }
